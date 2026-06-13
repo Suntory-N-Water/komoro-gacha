b767e80b56b166d571c0a56817fa04283b699053
sui <1202ayasnppk@gmail.com>
2026-06-13 09:37:10 +0900

    chore: 開発環境セットアップ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+const url =
+  import.meta.env.SITE ||
+  // TODO: 合ってるのかこれ？
+  import.meta.env["PUBLIC_APP_URL"] ||
+  "http://localhost:4321";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: url,
+  integrations: [
+    sitemap({
+      serialize(item) {
+        item.lastmod = new Date().toISOString();
+        return item;
+      },
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
